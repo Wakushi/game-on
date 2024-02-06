e332543988a1b42dc6d5b55cdc1c485d2f0cde80
makushi <maxime.eliazord@sequodia.com>
2024-02-06 11:10:29 +0100

    Form bindings and responsiveness fix

--- modal.js
+++ modal.js
@@ -2,11 +2,11 @@ class SignUpModalElement {
   constructor() {
     this.modal = document.querySelector(".bground")
     this.modalBtn = document.querySelectorAll(".modal-btn")
-    this.clodeModalBtn = document.querySelector(".close")
-    this._bindEvents()
+    this.clodeModalBtn = this.modal.querySelector(".close")
+    this.#bindEvents()
   }
 
-  _bindEvents() {
+  #bindEvents() {
     this.modalBtn.forEach((btn) =>
       btn.addEventListener("click", () => this.launchModal())
     )
@@ -15,161 +15,195 @@ class SignUpModalElement {
 
   launchModal() {
     this.modal.style.display = "block"
-    this.signUpForm = new SignUpFormElement()
+    App.toggleScroll()
+    if (!this.signUpForm) {
+      this.signUpForm = new SignUpFormElement()
+    }
   }
 
   closeModal() {
+    this.confirmationElement?.remove()
     this.modal.style.display = "none"
+    App.toggleScroll()
+  }
+
+  onFormSubmit(e) {
+    this.confirmationElement = document.createElement("div")
+    this.confirmationElement.classList.add("confirmation")
+    this.confirmationElement.innerHTML = `
+      <div class="confirmation">
+        <p>Merci pour votre inscription ${e.detail.firstName} !</p>
+        <button class="confirmation-close">Fermer</button>
+      </div>
+    `
+    this.modal
+      .querySelector(".modal-body")
+      .appendChild(this.confirmationElement)
+    this.confirmationElement
+      .querySelector(".confirmation-close")
+      .addEventListener("click", () => this.closeModal())
   }
 }
 
 class SignUpFormElement {
   constructor() {
-    this.formElement = document.querySelector(".signin-form")
-    this.userInputList = document.querySelectorAll(".text-control")
-    this.termsCheckbox = document.querySelectorAll("#checkbox1")
-    this.locationRadios = document.querySelectorAll("input[name='location']")
-    this.form = new Form("signin-form")
-    this._bindEvents()
-  }
-
-  _bindEvents() {
-    this.form.addEventListener("submit", (e) => this.onSubmit(e))
+    this.form = new Form(".signin-form")
   }
 }
 
 class Form {
-  errors = []
-
   constructor(formHook) {
     this.form = document.querySelector(formHook)
     this.#registerInputs()
+    this.#registerFields()
+    this.#bindEvents()
   }
 
   #registerInputs() {
     this.inputs = this.form.querySelectorAll("input")
-    console.log(this.inputs)
   }
 
-  isValid() {
-    this.errors = []
+  #registerFields() {
+    this.fields = []
+    this.inputs.forEach((input) => {
+      if (
+        !this.fields.find((field) => field.name === input.name) &&
+        input.name
+      ) {
+        this.fields.push({
+          name: input.name,
+          type: input.type,
+          required: input.name !== "newsletter",
+          value: null,
+          error: null,
+          parentNode: input.closest(".formData"),
+        })
+      }
+    })
+  }
+
+  #bindEvents() {
+    this.form.addEventListener("submit", (e) => this.#onSubmit(e))
+    this.inputs.forEach((input) => {
+      input.addEventListener("input", (e) => this.#onChange(e))
+      if (input.type !== "radio" && input.type !== "checkbox") {
+        input.addEventListener("blur", (e) =>
+          this.#checkFieldValidity(e.target.name)
+        )
+      }
+    })
+  }
+
+  #onChange(e) {
+    const { name, value, checked } = e.target
+    this.#saveValue(name, e.target.type === "checkbox" ? checked : value)
+    this.#checkFormValidity()
+  }
+
+  #saveValue(field, newValue) {
+    this.fields.map((registeredField) => {
+      if (registeredField.name === field) {
+        registeredField.value = newValue
+      }
+    })
+  }
+
+  #checkFieldValidity(fieldName) {
+    const field = this.fields.find((f) => f.name === fieldName)
+    if (!field) return
+    switch (field.type) {
+      case "text":
+        field.error =
+          field.value?.length < 2
+            ? "Le champs doit contenir au moins 2 caractères."
+            : null
+        break
+      case "email":
+        field.error = !field.value?.match(
+          /^[a-zA-Z0-9.#-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+        )
+          ? "L'email n'est pas valide"
+          : null
+        break
+      case "number":
+        field.error = !field.value ? "Veuillez choisir un nombre" : null
+        break
+      case "date":
+        field.error = !field.value ? "Veuillez choisir une date" : null
+        break
+      default:
+        break
+    }
+    if (field.error) {
+      field.parentNode.dataset.error = field.error
+      field.parentNode.dataset.errorVisible = true
+    } else {
+      field.parentNode.dataset.error = ""
+      field.parentNode.dataset.errorVisible = false
+    }
+  }
+
+  #checkFormValidity() {
     let isValid = true
-
-    if (!this._isFirstNameValid()) isValid = false
-    if (!this._isLastNameValid()) isValid = false
-    if (!this._isEmailValid()) isValid = false
-    if (!this._isContestAmountValid()) isValid = false
-    if (!this._isLocationValid()) isValid = false
-    if (!this._isTermsValid()) isValid = false
-    this._renderErrors()
-    return isValid
+    this.fields.forEach((field) => {
+      if (field.required && (field.error || !field.value)) {
+        isValid = false
+      }
+    })
+    this.#toggleSubmitButton(!isValid)
   }
 
-  _renderErrors() {
-    this.errors.forEach((error) => error.render())
+  #toggleSubmitButton(disabled) {
+    this.form.querySelector("button[type='submit']").disabled = disabled
   }
 
-  resetErrors() {
-    this.errors.forEach((error) => error.reset())
-    this.errors = []
+  #onSubmit(e) {
+    e.preventDefault()
+    const formData = {}
+    this.fields.forEach((field) => {
+      formData[field.name] = field.value
+    })
+    this.form.reset()
+    this.#toggleSubmitButton(true)
+    this.#emitFormSubmit(formData)
   }
 
-  _isFirstNameValid() {
-    if (!this.firstName.length || this.firstName.length < 2) {
-      this._registerFormError(new FormError("firstName"))
-      return false
-    }
-    return true
-  }
-
-  _isLastNameValid() {
-    if (!this.lastName.length || this.lastName.length < 2) {
-      this._registerFormError(new FormError("lastName"))
-      return false
-    }
-    return true
-  }
-
-  _isEmailValid() {
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
-    if (!this.email.match(emailRegex)) {
-      this._registerFormError(new FormError("email"))
-      return false
-    }
-    return true
-  }
-
-  _isContestAmountValid() {
-    if (!this.contestAmount) {
-      this._registerFormError(new FormError("contestAmount"))
-      return false
-    }
-    return true
-  }
-
-  _isLocationValid() {
-    if (!this.location) {
-      this._registerFormError(new FormError("location"))
-      return false
-    }
-    return true
-  }
-
-  _isTermsValid() {
-    if (!this.terms) {
-      this._registerFormError(new FormError("terms"))
-      return false
-    }
-    return true
-  }
-
-  _registerFormError(error) {
-    this.errors.push(error)
-    // const event = new CustomEvent("formError", { detail: this.errors })
-    // document.dispatchEvent(event)
+  #emitFormSubmit(formData) {
+    const event = new CustomEvent("formSubmit", { detail: formData })
+    document.dispatchEvent(event)
   }
 }
 
-class FormError {
-  //   constructor(field) {
-  //     this.field = field
-  //     this.message = this.getErrorMessage()
-  //     this.hook = document.querySelector(`.${field}-error`)
-  //   }
-  //   render() {
-  //     this.hook.innerText = this.message
-  //   }
-  //   reset() {
-  //     this.hook.innerText = ""
-  //   }
-  //   getErrorMessage() {
-  //     switch (this.field) {
-  //       case "firstName":
-  //         return "Le prénom doit contenir au moins 2 caractères"
-  //       case "lastName":
-  //         return "Le nom doit contenir au moins 2 caractères"
-  //       case "email":
-  //         return "L'email n'est pas valide"
-  //       case "contestAmount":
-  //         return "Veuillez choisir un nombre de concours"
-  //       case "location":
-  //         return "Veuillez choisir une ville"
-  //       case "terms":
-  //         return "Veuillez accepter les conditions d'utilisation"
-  //       default:
-  //         return "Erreur inconnue"
-  //     }
-  //   }
+class Header {
+  constructor() {
+    this.header = document.querySelector("header")
+    this.#bindEvents()
+  }
+
+  #bindEvents() {
+    this.header
+      .querySelector(".hamburger")
+      .addEventListener("click", () => this.#toggleNav())
+    this.header
+      .querySelector(".close")
+      .addEventListener("click", () => this.#toggleNav())
+  }
+
+  #toggleNav() {
+    this.header.querySelector(".main-navbar").classList.toggle("active")
+  }
 }
 
 class App {
   static init() {
-    new SignUpModalElement()
+    const signupModal = new SignUpModalElement()
+    document.addEventListener("formSubmit", (e) => signupModal.onFormSubmit(e))
+    new Header()
+  }
+
+  static toggleScroll() {
+    document.body.classList.toggle("no-scroll")
   }
 }
 
 App.init()
-
-document.addEventListener("modalLaunched", (e) => {
-  console.log("Modal launched", e.detail)
-})
